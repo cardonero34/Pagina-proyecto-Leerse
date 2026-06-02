b9d459af7c120d21f4f8a269c9c70bd0ad6b6470
Bbinnie6766 <manugg0112020@gmail.com>
2026-06-02 11:36:33 -0500

    groovyWalk manuela

--- src/components/LootieManuela.jsx
+++ src/components/LootieManuela.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { LootieManuela } from "./components/LootieManuela";
+import dataManuela from '../assets/dataManuela.json'
+import groovyWalk from "./groovyWalk.json";
 
 export const LootieManuela = () => {
   return (
-    <div>LootieManuela</div>
+    <div>
+        <Lottie
+        animationData={dataManuela}
+        loop={true}
+        style={{ width:500 , height:500 }}
+        />
+    </div>
   )
 }
