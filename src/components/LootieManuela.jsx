541849803d58b11ca1b7861e7fe7d6c24e32afc7
Bbinnie6766 <manugg0112020@gmail.com>
2026-06-03 11:12:05 -0500

    nuevas carpetas

--- src/components/LootieManuela.jsx
+++ src/components/LootieManuela.jsx
@@ -1,15 +1,31 @@
-import { LootieManuela } from "./components/LootieManuela";
+import LottieModule from "lottie-react";
 import dataManuela from '../assets/dataManuela.json'
-import groovyWalk from "./groovyWalk.json";
+import { useRef } from "react";
+
+const Lottie = LottieModule.default;
 
 export const LootieManuela = () => {
+
+  const lottieRef = useRef()
+
+  const reproducir = () => {
+    lottieRef.current.stop()
+    lottieRef.current.play()
+  }
+
   return (
-    <div>
+    <>
+      <div onClick={reproducir}>
+
         <Lottie
-        animationData={dataManuela}
-        loop={true}
-        style={{ width:500 , height:500 }}
+          lottieRef={lottieRef}
+          animationData={dataManuela}
+          loop={false}
+          autoplay={false}
+          
+          style={{ width: 500, height: 500 }}
         />
-    </div>
+      </div>
+    </>
   )
 }
