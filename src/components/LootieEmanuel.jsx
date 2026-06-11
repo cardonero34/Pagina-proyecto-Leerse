b7734cd97c9c10fbed298face99824aa49192639
cardonero34 <produccionmultimediaemanuel@gmail.com>
2026-06-11 10:15:31 -0500

    pinguino

--- src/components/LootieEmanuel.jsx
+++ src/components/LootieEmanuel.jsx
@@ -1,5 +1,7 @@
 import LottieModule from "lottie-react"
 import PelotaEman from '../assets/dataEmanuel.json'
+import OsoEMA from '../assets/osoEMANUEL.json'
+import Penguin from '../assets/penguin.json'
 import { useRef } from "react"
 
 const Lottie = LottieModule.default;
@@ -7,22 +9,56 @@ const Lottie = LottieModule.default;
 export const LootieEmanuel = () => {
 
   const lottieRef = useRef()
+  const lottieRef2 = useRef()
+  const lottieRef3 = useRef()
 
   const reproducir = () => {
     lottieRef.current.stop()
     lottieRef.current.play()
   }
 
+  const play = () => {
+    lottieRef2.current.stop()
+    lottieRef2.current.play()
+  }
+
+  const penguinplay = () => {
+    lottieRef3.current.stop()
+    lottieRef3.current.play()
+  }
+
   return (
     <>
-      <div onClick={reproducir} >
-        <Lottie
-          lottieRef={lottieRef}
-          animationData={PelotaEman}
-          loop={false}
-          autoplay={false}
-          style={{ width: 800, height: 800 }}
-        />
+      <div className="d-flex">
+        <div onClick={reproducir} >
+          <Lottie
+            lottieRef={lottieRef}
+            animationData={PelotaEman}
+            loop={false}
+            autoplay={false}
+            style={{ width: 500, height: 500 }}
+          />
+        </div>
+
+        <div onClick={play}>
+          <Lottie
+            lottieRef={lottieRef2}
+            animationData={OsoEMA}
+            loop={false}
+            autoplay={false}
+            style={{ width: 500, height: 500 }}
+          />
+        </div>
+
+        <div onClick={penguinplay}>
+          <Lottie
+            lottieRef={lottieRef3}
+            animationData={Penguin}
+            loop={false}
+            autoplay={false}
+            style={{ width: 500, height: 500 }}
+          />
+        </div>
       </div>
     </>
 
